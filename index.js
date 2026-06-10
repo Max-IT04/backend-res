8f6c3d02e9d22a18f432056e43dee55b31dd552a
Maxim Roschin <maks.roshin2004@gmail.com>
2026-06-10 23:02:42 +0300

    Основы задание 1

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-const yargs = require('yargs/yargs');
-const { hideBin } = require('yargs/helpers');
-const pkg = require('./package.json');
-const { addNote, printNotes, removeNote } = require('./notes-controller');
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const pkg = require("./package.json");
+const { addNote, printNotes, removeNote } = require("./notes-controller");
 
-const argv = yargs(hideBin(process.argv))
+yargs(hideBin(process.argv))
   .version(pkg.version)
   .command({
-    command: 'add',
-    describe: 'Add new note list',
+    command: "add",
+    describe: "Add new note list",
     builder: {
       title: {
-        type: 'string',
-        describe: 'Note title',
-        demandOption: true
-      }
+        type: "string",
+        describe: "Note title",
+        demandOption: true,
+      },
     },
-    handler(title) {
-      addNote(title)
-    }
+    handler(argv) {
+      addNote(argv.title);
+    },
   })
   .command({
-    command: 'list',
-    describe: 'Print all notes',
-    async handler() {
-      async function printNotes() {
-        printNotes();
-      }}
+    command: "list",
+    describe: "Print all notes",
+    handler() {
+      printNotes();
+    },
   })
   .command({
-    command: 'remove',
-    describe: 'Print note by id',
+    command: "remove",
+    describe: "Remove note by id",
     builder: {
       id: {
-        type: 'string',
-        describe: 'Note uniq id',
-        demandOption: true
-      }
+        type: "string",
+        describe: "Note uniq id",
+        demandOption: true,
+      },
     },
-    async handler({ id }) {
-      removeNote();
-    }
+    handler(argv) {
+      removeNote(argv.id);
+    },
   })
   .parse();
-
-console.log(argv);
